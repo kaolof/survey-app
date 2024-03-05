8e301120e140e28162fc01bb4b65675e4f2a8a31
Kao <kaochilanlo@gmail.com>
2024-03-04 22:36:28 -0400

    adding migrations

---
+++ database/migrations/2024_03_05_014309_create_responses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('survey_id');
+            $table->text('value');
+            $table->timestamps();
+    
+            $table->foreign('user_id')->references('id')->on('users');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+};
